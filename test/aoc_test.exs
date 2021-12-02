b93e2b8e70eff48c5646ed2a4cd3c6470b29b956
BJ Bryant <bbryant@covermymeds.com>
2021-12-01 21:48:34 -0600

    clean up

--- test/aoc_test.exs
+++ test/aoc_test.exs
@@ -2,9 +2,6 @@ defmodule AocTest do
   use ExUnit.Case
   doctest Aoc
 
-  # test "greets the world" do
-  #   assert Aoc.hello() == :world
-  # end
   test "runs with input" do
     Aoc.run()
   end
