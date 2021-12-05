78c1fdad5561fa2a61d8f7ba8d077745f9ecf49e
BJ Bryant <bryantb76@gmail.com>
2021-12-05 00:24:48 -0600

    05-a-complete

--- lib/aoc.ex
+++ lib/aoc.ex
@@ -1,28 +1,4 @@
 defmodule Aoc do
-  @doc """
-  ## Examples
-
-      iex> Aoc.run("7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
-      ...>
-      ...>22 13 17 11  0
-      ...>8  2 23  4 24
-      ...>21  9 14 16  7
-      ...>6 10  3 18  5
-      ...>1 12 20 15 19
-      ...>
-      ...>3 15  0  2 22
-      ...>9 18 13 17  5
-      ...>19  8  7 25 23
-      ...>20 11 10 24  4
-      ...>14 21 16 12  6
-      ...>
-      ...>14 21 17 24  4
-      ...>10 16 15  9 19
-      ...>18  8 23 26 20
-      ...>22 11 13  6  5
-      ...>2  0 12  3  7")
-      1924
-  """
   def run(input \\ nil)
 
   def run(input) when is_nil(input) do
@@ -32,100 +8,42 @@ defmodule Aoc do
   end
 
   def run(input) do
-    game = build_game(input)
-
-    game
-    |> draw_until_win()
-    |> find_winner()
-    |> score_card(game)
+    input
+    |> clean()
+    |> only_horizontal_and_vertical()
+    |> Stream.map(&plot_line/1)
+    |> Enum.to_list()
+    |> List.flatten()
+    |> Enum.frequencies()
+    |> Enum.filter(fn {_, count} -> count >= 2 end)
+    |> length
     |> IO.inspect()
   end
 
-  def build_game(string) do
-    [draws | boards] = String.split(string, "\n\n", trim: true)
-    boards = build_board(boards)
-
-    draws = Regex.scan(~r/\d+/, draws) |> List.flatten()
-
-    %{:draws => draws, :boards => boards}
-  end
-
-  def build_board(boards) when is_list(boards) do
-    boards
-    |> Stream.map(&build_board/1)
-    |> Enum.to_list()
-  end
-
-  def build_board(board) do
-    board
+  def clean(input) do
+    input
     |> String.split("\n", trim: true)
-    |> Stream.map(&Regex.scan(~r/\d+\b/, &1))
-    |> Enum.to_list()
-    |> List.flatten()
+    |> Stream.map(&String.split(&1, " -> ", trim: true))
+    |> Stream.map(&Enum.map(&1, fn s -> String.trim(s) end))
+    |> Stream.map(&Enum.map(&1, fn s -> String.split(s, ",") end))
+    |> Stream.map(&List.flatten/1)
+    |> Stream.map(&Enum.map(&1, fn s -> String.to_integer(s) end))
   end
 
-  def draw(num, board) do
-    idx = Enum.find_index(board, &(&1 == num))
-    board = (idx && List.replace_at(board, idx, ".")) || board
-
-    case idx do
-      nil -> {board, {0, 0}}
-      _ -> {board, place_makes_win?(idx, board)}
-    end
+  def only_horizontal_and_vertical(list) do
+    list
+    |> Stream.filter(fn [x1, y1, x2, y2] -> x1 == x2 || y1 == y2 end)
   end
 
-  def draw_until_win(game) do
-    Util.stream_reduce(game.boards, [], fn board, acc ->
-      Enum.reduce_while(game.draws, board, fn num, acc_board ->
-        draw_result = draw(num, acc_board)
-
-        case draw_result do
-          {board, {1, _}} -> {:halt, {Enum.find_index(game.draws, &(&1 == num)), board}}
-          {board, {_, 1}} -> {:halt, {Enum.find_index(game.draws, &(&1 == num)), board}}
-          {board, _} -> {:cont, board}
-        end
-      end)
-      |> (&[&1 | acc]).()
+  def plot_line([x, y1, x, y2]) do
+    Enum.reduce(y1..y2, [], fn el, acc ->
+      [{x, el} | acc]
     end)
   end
 
-  # The given place `n` is a win if all
-  # the the spots in `n`'s row or column
-  # are marked.
-  def place_makes_win?(n, board) do
-    row_win =
-      board
-      |> Enum.slice(div(n, 5) * 5, 5)
-      |> Enum.all?(&(&1 == "."))
-      |> (&((&1 && 1) || 0)).()
-
-    col_win =
-      Util.stream_reduce(0..4, [], fn row, acc ->
-        [Enum.at(board, row * 5 + rem(n, 5)) | acc]
-      end)
-      |> Enum.all?(&(&1 == "."))
-      |> (&((&1 && 1) || 0)).()
-
-    {row_win, col_win}
-  end
-
-  def find_winner(results) do
-    results
-    |> Stream.map(&Tuple.to_list/1)
-    |> Enum.sort_by(&Enum.at(&1, 0))
-    |> Enum.reverse()
-    |> hd
-  end
-
-  def score_card([idx, board], %{draws: draws}) do
-    unmarked =
-      board
-      |> Enum.filter(&(&1 != "."))
-      |> Enum.map(&String.to_integer/1)
-      |> Enum.sum()
-
-    Enum.at(draws, idx)
-    |> String.to_integer()
-    |> (&(&1 * unmarked)).()
+  def plot_line([x1, y, x2, y]) do
+    Enum.reduce(x1..x2, [], fn el, acc ->
+      [{el, y} | acc]
+    end)
   end
 end
--- test/aoc_test.exs
+++ test/aoc_test.exs
@@ -2,6 +2,21 @@ defmodule AocTest do
   use ExUnit.Case
   doctest Aoc
 
+  test "runs with test input" do
+    result = Aoc.run("0,9 -> 5,9
+    8,0 -> 0,8
+    9,4 -> 3,4
+    2,2 -> 2,1
+    7,0 -> 7,4
+    6,4 -> 2,0
+    0,9 -> 2,9
+    3,4 -> 1,4
+    0,0 -> 8,8
+    5,5 -> 8,2")
+
+    assert result == 5
+  end
+
   test "runs with input" do
     Aoc.run()
   end
